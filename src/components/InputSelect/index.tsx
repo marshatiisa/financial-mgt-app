f58a92da3c0f92e6dced97b9abe2d7dbc55a23b1
Marsha Tiisa <100469351+marshatiisa@users.noreply.github.com>
2023-05-05 12:39:27 -0400

    Initial commit

---
+++ src/components/InputSelect/index.tsx
@@ -0,0 +1,122 @@
+import Downshift from "downshift"
+import { useCallback, useState } from "react"
+import classNames from "classnames"
+import styled from "styled-components"
+import { InputSelectOnChange, InputSelectProps } from "./types"
+
+const DropDown = styled.div<{ top: number; left: number }>`
+  position: absolute;
+  top: ${({ top }) => top};
+  left: ${({ left }) => left};
+`
+
+export function InputSelect<TItem>({
+  label,
+  defaultValue,
+  onChange: consumerOnChange,
+  items,
+  parseItem,
+  isLoading,
+  loadingLabel,
+}: InputSelectProps<TItem>) {
+  const [selectedValue, setSelectedValue] = useState<TItem | null>(defaultValue ?? null)
+  const [dropdownParent, setDropdownParent] = useState<HTMLElement | null>(null)
+
+  const onChange = useCallback<InputSelectOnChange<TItem>>(
+    (selectedItem) => {
+      if (selectedItem === null) {
+        return
+      }
+
+      consumerOnChange(selectedItem)
+      setSelectedValue(selectedItem)
+    },
+    [consumerOnChange]
+  )
+
+  return (
+    <Downshift<TItem>
+      id="RampSelect"
+      onChange={onChange}
+      selectedItem={selectedValue}
+      itemToString={(item) => (item ? parseItem(item).label : "")}
+    >
+      {({
+        getItemProps,
+        getLabelProps,
+        getMenuProps,
+        isOpen,
+        highlightedIndex,
+        selectedItem,
+        getToggleButtonProps,
+        inputValue,
+      }) => {
+        const toggleProps = getToggleButtonProps()
+        const parsedSelectedItem = selectedItem === null ? null : parseItem(selectedItem)
+
+        function renderItems() {
+          if (!isOpen) {
+            return null
+          }
+
+          if (isLoading) {
+            return <div className="RampInputSelect--dropdown-item">{loadingLabel}...</div>
+          }
+
+          if (items.length === 0) {
+            return <div className="RampInputSelect--dropdown-item">No items</div>
+          }
+
+          return items.map((item, index) => {
+            const parsedItem = parseItem(item)
+            return (
+              <div
+                key={parsedItem.value}
+                {...getItemProps({
+                  key: parsedItem.value,
+                  index,
+                  item,
+                  className: classNames("RampInputSelect--dropdown-item", {
+                    "RampInputSelect--dropdown-item-highlighted": highlightedIndex === index,
+                    "RampInputSelect--dropdown-item-selected":
+                      parsedSelectedItem?.value === parsedItem.value,
+                  }),
+                })}
+              >
+                {parsedItem.label}
+              </div>
+            )
+          })
+        }
+
+        return (
+          <div className="RampInputSelect--root">
+            <label className="RampText--s RampText--hushed" {...getLabelProps()}>
+              {label}
+            </label>
+            <div className="RampBreak--xs" />
+            <div
+              className="RampInputSelect--input"
+              onClick={(event) => {
+                toggleProps.onClick(event)
+                setDropdownParent(event.currentTarget)
+              }}
+            >
+              {inputValue}
+            </div>
+            <DropDown
+              className={classNames("RampInputSelect--dropdown-container", {
+                "RampInputSelect--dropdown-container-opened": isOpen,
+              })}
+              {...getMenuProps()}
+              top={dropdownParent ? dropdownParent.getBoundingClientRect().bottom : 0}
+              left={dropdownParent ? dropdownParent.getBoundingClientRect().left : 0}
+            >
+              {renderItems()}
+            </DropDown>
+          </div>
+        )
+      }}
+    </Downshift>
+  )
+}
